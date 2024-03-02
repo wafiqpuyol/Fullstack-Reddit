862c62e669bb9f6fbc44a51df2575027ec4a413e
wafiq <wafiqgithub775@gmail.com>
2024-03-02 06:40:57 +0600

    FIX: changed the import statement

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,15 @@
-import type { JWT } from "next-auth/jwt";
-import { User } from "next-auth";
-declare module "next/auth" {
+import type { Session, JWT } from "next-auth/jwt";
+import type { User } from "next-auth";
+
+type UserId = string;
+declare module "next-auth/jwt" {
   interface JWT {
     id: UserId;
     username?: string | null;
   }
 }
 
-declare module "next/auth" {
+declare module "next-auth" {
   interface Session {
     user: User & {
       id: UserId;
