2758bf3f9e315c497437fb4bc1cfcb67e8839ff0
wafiq <wafiqgithub775@gmail.com>
2024-03-02 11:14:45 +0600

    added text editor & uploadthing

--- src/app/(searchsubreddit)/r/[slug]/submit/page.tsx
+++ src/app/(searchsubreddit)/r/[slug]/submit/page.tsx
@@ -1,9 +1,15 @@
+import { Editor } from "@/components/Editor";
 import SearchSubreddit from "@/components/SearchSubreddit";
+import { Button } from "@/components/ui/Button";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
-import React from "react";
+import { notFound } from "next/navigation";
 
-const page = async ({ params: { slug } }: { params: { slug: string } }) => {
+const page = async ({
+  params: { slug: subredditName },
+}: {
+  params: { slug: string };
+}) => {
   const session = await getAuthSession();
   const subscriptions = await db.subscription.findMany({
     where: {
@@ -19,10 +25,20 @@ const page = async ({ params: { slug } }: { params: { slug: string } }) => {
       },
     },
   });
+  const subreddit = await db.subreddit.findFirst({
+    where: {
+      name: subredditName,
+    },
+  });
 
+  if (!subreddit) notFound();
   return (
     <div>
-      <SearchSubreddit user={session?.user} subscription={subscriptions} />
+      <SearchSubreddit subscription={subscriptions} />
+      <Editor subredditId={subreddit.id} />
+      <Button type="submit" className="w-full" form="subreddit-post-form">
+        Post
+      </Button>
     </div>
   );
 };
---
+++ src/components/Editor.tsx
@@ -0,0 +1,236 @@
+"use client";
+
+import EditorJS from "@editorjs/editorjs";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { usePathname, useRouter } from "next/navigation";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import TextareaAutosize from "react-textarea-autosize";
+import { z } from "zod";
+
+import { toast } from "@/hooks/use-toast";
+import { uploadFiles } from "@/lib/uploadthing";
+import { createPostPayload, createPostValidator } from "@/lib/validation/post";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+import "@/styles/editor.css";
+import { Input } from "./ui/Input";
+
+type FormData = z.infer<typeof createPostValidator>;
+
+interface EditorProps {
+  subredditId: string;
+}
+
+const maxInputLength = 20;
+export const Editor: React.FC<EditorProps> = ({ subredditId }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(createPostValidator),
+    defaultValues: {
+      subredditId,
+      title: "",
+      content: null,
+    },
+  });
+  const ref = useRef<EditorJS>();
+  const _titleRef = useRef<HTMLTextAreaElement>(null);
+  const router = useRouter();
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const pathname = usePathname();
+  const [wordLength, setWordLength] = useState(maxInputLength);
+  const [input, setInput] = useState("");
+
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({ title, content, subredditId }: createPostPayload) => {
+      const payload: createPostPayload = { title, content, subredditId };
+      const { data } = await axios.post("/api/subreddit/post/create", payload);
+      return data;
+    },
+    onError: () => {
+      return toast({
+        title: "Something went wrong.",
+        description: "Your post was not published. Please try again.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      // turn pathname /r/mycommunity/submit into /r/mycommunity
+      const newPathname = pathname.split("/").slice(0, -1).join("/");
+      router.push(newPathname);
+
+      router.refresh();
+
+      return toast({
+        description: "Your post has been published.",
+      });
+    },
+  });
+
+  const initializeEditor = useCallback(async () => {
+    const EditorJS = (await import("@editorjs/editorjs")).default;
+    const Header = (await import("@editorjs/header")).default;
+    const Embed = (await import("@editorjs/embed")).default;
+    const Table = (await import("@editorjs/table")).default;
+    const List = (await import("@editorjs/list")).default;
+    const Code = (await import("@editorjs/code")).default;
+    const LinkTool = (await import("@editorjs/link")).default;
+    const InlineCode = (await import("@editorjs/inline-code")).default;
+    const ImageTool = (await import("@editorjs/image")).default;
+
+    if (!ref.current) {
+      const editor = new EditorJS({
+        holder: "editor",
+        onReady() {
+          ref.current = editor;
+        },
+        placeholder: "Type here to write your post...",
+        inlineToolbar: true,
+        data: { blocks: [] },
+        tools: {
+          header: Header,
+          linkTool: {
+            class: LinkTool,
+            config: {
+              endpoint: "/api/link",
+            },
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                async uploadByFile(file: File) {
+                  // upload to uploadthing
+                  const [res] = await uploadFiles([file], "imageUploader");
+
+                  return {
+                    success: 1,
+                    file: {
+                      url: res.fileUrl,
+                    },
+                  };
+                },
+              },
+            },
+          },
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          table: Table,
+          embed: Embed,
+        },
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(errors).length) {
+      for (const [_key, value] of Object.entries(errors)) {
+        value;
+        toast({
+          title: "Something went wrong.",
+          description: (value as { message: string }).message,
+          variant: "destructive",
+        });
+      }
+    }
+  }, [errors]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setIsMounted(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor();
+
+      setTimeout(() => {
+        _titleRef?.current?.focus();
+      }, 0);
+    };
+
+    if (isMounted) {
+      init();
+
+      return () => {
+        ref.current?.destroy();
+        ref.current = undefined;
+      };
+    }
+  }, [isMounted, initializeEditor]);
+
+  async function onSubmit(data: FormData) {
+    const blocks = await ref.current?.save();
+
+    const payload: createPostPayload = {
+      title: data.title,
+      content: blocks,
+      subredditId,
+    };
+    createPost(payload);
+  }
+
+  if (!isMounted) {
+    return null;
+  }
+
+  const { ref: titleRef, ...rest } = register("title");
+
+  return (
+    <div className="w-full p-4 bg-primary rounded-lg border border-primary mb-4 ">
+      <div
+        className="bg-primary flex w-full gap-4 items-center border-2 rounded-md 
+       border-zinc-700 "
+      >
+        <Input
+          ref={(e) => titleRef(e)}
+          placeholder="Please write on me"
+          className="text-zinc-200 border-none  bg-transparent  focus-visible:ring-offset-0 focus-visible:ring-0 selection:bg-blue-600"
+          maxLength={maxInputLength}
+          onChange={(e) => {
+            setInput((textAreaValue: string) => {
+              setWordLength((textAreaLength: number) => {
+                const inputLength = e.target.value.length;
+                if (inputLength <= 0) {
+                  return maxInputLength;
+                }
+                if (e.target.value.length - textAreaValue.length > 0) {
+                  return textAreaLength - 1;
+                }
+                if (e.target.value.length - textAreaValue.length < 0) {
+                  return textAreaLength + 1;
+                }
+              });
+
+              return e.target.value;
+            });
+          }}
+          value={input}
+        />
+        <p className="text-zinc-400 w-16 text-xs font-bold">{`${wordLength} / ${maxInputLength}`}</p>
+      </div>
+      <form
+        id="subreddit-post-form"
+        className="w-fit"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="prose prose-stone dark:prose-invert text-zinc-200 selection:bg-blue-600">
+          <div id="editor" className="min-h-[500px] text-zinc-200" />
+          <p className="text-sm text-zinc-400">
+            Use{" "}
+            <kbd className="rounded-md border bg-muted px-1 text-xs uppercase">
+              Tab
+            </kbd>{" "}
+            to open the command menu.
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
