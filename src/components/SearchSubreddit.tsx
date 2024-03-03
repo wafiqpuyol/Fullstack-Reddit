d19544c71bc9933494a829cbd7d044e4ec7ff6f0
wafiq <wafiqgithub775@gmail.com>
2024-03-03 10:25:55 +0600

    added session provider

--- src/components/SearchSubreddit.tsx
+++ src/components/SearchSubreddit.tsx
@@ -2,24 +2,25 @@
 
 import React, { FC, useContext, useEffect, useState } from "react";
 import { Input } from "./ui/Input";
-import { Subscription, User } from "@prisma/client";
+import { Subscription } from "@prisma/client";
 import UserAvatar from "./UserAvatar";
 import CreateCommunityDialog from "./CreateCommunityDialog";
 import { usePathname, useRouter } from "next/navigation";
 import { Search } from "lucide-react";
 import { InputContext } from "../context/Input";
+import { useSession } from "next-auth/react";
+import { ChevronDown } from "lucide-react";
 interface SearchSubredditProp {
-  subscription: Subscription &
-    {
-      subreddit: {
-        id: string;
-        name: string;
-        subscribers: Subscription[];
-      };
-    }[];
-  user: Pick<User, "image" | "username">;
+  subscription: (Subscription & {
+    subreddit: {
+      id: string;
+      name: string;
+      subscribers: Subscription[];
+    };
+  })[];
 }
-const SearchSubreddit: FC<SearchSubredditProp> = ({ subscription, user }) => {
+const SearchSubreddit: FC<SearchSubredditProp> = ({ subscription }) => {
+  const { data } = useSession();
   const { subreddit, setSubreddit } = useContext(InputContext);
   const [dropDownOpen, setDropDownOpen] = useState(false);
   const pathname = usePathname();
@@ -31,20 +32,23 @@ const SearchSubreddit: FC<SearchSubredditProp> = ({ subscription, user }) => {
   return (
     // ((pathname.startsWith("/r") && pathname.endsWith("/submit")) ||
     //   pathname.startsWith("/submit")) && (
-    <div className="flex flex-col">
-      <div className="flex items-center bg-zinc-800 px-2 rounded-md">
+    <div className="flex flex-col w-64 mb-3">
+      <h4 className="mb-5">Create a post</h4>
+      <hr className="border-zinc-700" />
+      <div className="flex items-center bg-zinc-900 px-2 mt-5 rounded-tr-md rounded-tl-md">
         <Search />
         <Input
           placeholder="Search communities"
           className="bg-transparent placeholder:text-zinc-400 focus-visible:border-0 focus-visible:ring-0  focus-visible:ring-offset-0 ring-offset-0 border-0"
           onClick={() => setDropDownOpen((prev) => !prev)}
           onChange={(e) => setSubreddit(e.target.value)}
-          value={subreddit}
+          value={subreddit ?? pathname.split("/")[2]}
         />
+        <ChevronDown size={22} className="text-zinc-400" />
       </div>
       {dropDownOpen && (
         <div
-          className="w-72 max-h-52 overflow-y-scroll bg-primary"
+          className="w-64 max-h-52 overflow-y-scroll bg-zinc-900 border-t-[1.5px] border-t-zinc-700 rounded-bl-md rounded-br-md"
           id="dropdown"
         >
           <div className="px-2 py-1">
@@ -52,11 +56,14 @@ const SearchSubreddit: FC<SearchSubredditProp> = ({ subscription, user }) => {
               Your profile
             </span>
             <div className="flex items-center gap-2 mt-1">
-              <UserAvatar profileImage={user.image} width="w-7"></UserAvatar>
-              <span className="text-sm">u/{user.username}</span>
+              <UserAvatar
+                profileImage={data.user.image}
+                width="w-7"
+              ></UserAvatar>
+              <span className="text-sm">u/{data.user.username}</span>
             </div>
           </div>
-          <hr className="my-3" />
+          <hr className="my-3 border-zinc-700" />
 
           <div className="px-2 py-1">
             <div className="flex justify-between items-center mb-3">
@@ -73,7 +80,10 @@ const SearchSubreddit: FC<SearchSubredditProp> = ({ subscription, user }) => {
               {subscription?.map(({ subreddit }) => (
                 <li
                   key={subreddit.id}
-                  onClick={() => router.push(`r/${subreddit.name}/submit`)}
+                  onClick={() => {
+                    setSubreddit(subreddit.name);
+                    router.push(`r/${subreddit.name}/submit`);
+                  }}
                   className="cursor-pointer text-sm font-semibold flex flex-col justify-center text-zinc-300 w-full hover:bg-zinc-900"
                 >
                   {`r/${subreddit.name}`}
--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -2,6 +2,7 @@
 
 import React, { FC } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +11,9 @@ interface ProviderProp {
 }
 const Provider: FC<ProviderProp> = ({ children }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
   );
 };
 
